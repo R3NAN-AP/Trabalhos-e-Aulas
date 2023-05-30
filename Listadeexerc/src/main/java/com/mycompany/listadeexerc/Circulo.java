b7a70422e60c34f36c26c8daf02d2ea318cd444a
Renan Alves Peregrino <126730336+R3NAN-AP@users.noreply.github.com>
2023-05-30 20:34:56 -0300

    Add files via upload

---
+++ Listadeexerc/src/main/java/com/mycompany/listadeexerc/Circulo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.listadeexerc;
+
+/**
+ *
+ * @author aluno
+ */
+public class Circulo implements Desenhavel{
+
+    @Override
+    public void desenhar() {
+        System.out.println(" Desenhando Circulo");
+    }
+    
+}
---
+++ Listadeexerc/src/main/java/com/mycompany/listadeexerc/Exercicio1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listadeexerc;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Triangulo triangulo = new Triangulo();
+        Retangulo retangulo = new Retangulo();
+        Circulo circulo = new Circulo();
+        
+        circulo.desenhar();
+        triangulo.desenhar();
+        retangulo.desenhar();
+    }
+}
